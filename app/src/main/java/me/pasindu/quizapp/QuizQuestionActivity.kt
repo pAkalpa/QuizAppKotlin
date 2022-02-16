05c7b0b47d2e1b8f3fc34ecc436d35421dd8cad9
pAkalpa <pasinduakalpa1998@gmail.com>
2022-02-16 13:19:10 +0530

    :art: questions update fixed

--- app/src/main/java/me/pasindu/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/me/pasindu/quizapp/QuizQuestionActivity.kt
@@ -15,5 +15,21 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is", "${questionsList.size}")
+
+        for (i in questionsList) {
+            Log.e("Question", i.question)
+        }
+        var currentPosition = 1
+        val question: Question = questionsList[currentPosition - 1]
+        binding.ivImage.setImageResource(question.image)
+        binding.progressBar.progress = currentPosition
+        binding.progress.text = "${currentPosition}/${binding.progressBar.max}"
+        binding.tvQuestion.text = question.question
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
+
+
     }
 }
