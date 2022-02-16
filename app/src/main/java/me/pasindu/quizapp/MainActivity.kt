af705ffa9202768731eafe1042731e1bcc296f4e
pAkalpa <pasinduakalpa1998@gmail.com>
2022-02-16 13:05:08 +0530

    :art: Finalize Quiz page

--- app/src/main/java/me/pasindu/quizapp/MainActivity.kt
+++ app/src/main/java/me/pasindu/quizapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package me.pasindu.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import me.pasindu.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val contentView = binding.root
         setContentView(contentView)
+
+        val etName: EditText = findViewById(R.id.et_name)
+
+        binding.btnStart.setOnClickListener {
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
